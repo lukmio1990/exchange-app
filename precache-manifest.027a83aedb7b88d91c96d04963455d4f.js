419f202d27f5ebfae829e04cba4d60f703c2a568
Łukasz Mioduszewski <lukasz.mioduszewski1990@wp.pl>
2019-06-05 19:27:45 +0200

    Updates

---
+++ precache-manifest.027a83aedb7b88d91c96d04963455d4f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132a6acdcfe6f9720a046d673a86ede0",
+    "url": "/exchange-app/index.html"
+  },
+  {
+    "revision": "1a027a9dbe50423e740e",
+    "url": "/exchange-app/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "f8059525735e68566af2",
+    "url": "/exchange-app/static/js/2.d70a297a.chunk.js"
+  },
+  {
+    "revision": "1a027a9dbe50423e740e",
+    "url": "/exchange-app/static/js/main.444af463.chunk.js"
+  },
+  {
+    "revision": "43d78a810dc729a1c474",
+    "url": "/exchange-app/static/js/runtime~main.80d10873.js"
+  },
+  {
+    "revision": "f60d22b31ea6b339417245ae9ad4202f",
+    "url": "/exchange-app/static/media/exchangeIcon.f60d22b3.png"
+  },
+  {
+    "revision": "985ff3f92cd8cdcecbf1e3db4d7fba8e",
+    "url": "/exchange-app/static/media/historyIcon.985ff3f9.png"
+  },
+  {
+    "revision": "7501904d5225514e0c8a80df0869eba0",
+    "url": "/exchange-app/static/media/logo.7501904d.png"
+  },
+  {
+    "revision": "16e3603c2651f6301b3f4b40b3e56f3a",
+    "url": "/exchange-app/static/media/money2.16e3603c.jpg"
+  }
+]);
